49e6dbe71df16ba7b1c6813616fe4b76dd45b100
toan032 <85279387+toan032@users.noreply.github.com>
2021-06-03 15:56:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hân ơiiiiii <3!',
+    introTitle: 'ccccccccc <3!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
