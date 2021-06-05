b5771b7f9fc422e03ae49d2fdd5300cf112588c3
toan032 <85279387+toan032@users.noreply.github.com>
2021-06-05 21:52:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'alo lớp trưởng  <3!',
+    introTitle: 'Hân ơiiiiiiii  <3!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'mốt hết dịch nhớ đem cua biển sò nghêu ốc mực bạch tuột vô nha  😙',
-    desc: 'đem càng nhìu càng tốt nha kkkkkkkkk ',
-    btnYes: 'oke bẹn tòn  <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'lâu quá không gặp lớp trưởng mà nào vô lai thành phố nhớ đem đồ hải sản vô nha kkk <3',
+    title: 'có thèm trà sữa hông nè !!!  😙',
+    desc: 'trả lời thiệt nhaaaaaaaaaaaaa ',
+    btnYes: 'Hân rất thefmmmmm muốn uống lắm nha  <33',
+    btnNo: 'Không Hân hông có nha ! :3',
+    question:'lâu quá không gặp Haann nhớ hân ghê @@ kaka ',
     btnReply: 'Gửi cho bẹn tòn  <3',
-    reply: 'ok tui sẽ đem cho bẹn rất nhìu nha <33333333',
-    mess: 'ở nhà vui vẻ nha lớp trưởng 😘😘',
-    messDesc: 'nhớ đem hải sản cho nhìu nha ',
+    reply: 'điền không được đầu đừng có điền gì hết á  <33333333',
+    mess: 'tui biết Hân rất nhớ hương vị của trà sữa kkk 😘😘',
+    messDesc: 'nào thèm cứ nói Hồm nay Hân thèm trà sữa :333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100021184031768' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
